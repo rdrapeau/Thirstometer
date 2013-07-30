75e050f67d555ab73d1d16815bebf6463cfacdcd
Ryan Drapeau <rdrapeau1@gmail.com>
2013-07-30 15:52:27 -0700

    Logging

--- src/app/js/scripts.js
+++ src/app/js/scripts.js
@@ -113,10 +113,12 @@
 			console.log("drawig");
 			if (data.success) {
 			 	var graph = new google.visualization.DataTable();
+			 	console.log("Adding column");
 			  	graph.addColumn('date', 'Date');
 			   	graph.addColumn('number', 'Cups');
 			   	var graphData = [];
 				for (var i = 0; i < data.result.length; i++) {
+					console.log("Adding Row");
 					graphData.push([new Date(data.result[i].year, data.result[i].month, data.result[i].day), data.result[i].data.amount * 1]);
 				}
 				graph.addRows(graphData);
