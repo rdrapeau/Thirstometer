c0a6808a7f6efc30a1d377bc39807aa5a677f111
Aaron Nech <aaron@localhost.localdomain>
2013-07-30 12:06:52 -0700

    added drink framework for js

--- src/app/js/scripts.js
+++ src/app/js/scripts.js
@@ -1,3 +1,4 @@
+//login functionality, start app here
 (function() {
 	var host = "http://173.255.120.22:1337";
 
@@ -22,5 +23,38 @@
 		} else {
 			
 		}
+	}
+
+
+	function mainApp(user) {
+		var userObject = user;
+		$("#main").show();
+		$(".drinkingButton").click(handleDrinkPress);
+
+		function handleDrinkPress() {
+			var amount = $(this).data('amount');
+			sendDrinkTransaction({"amount" : amount}, function(data) {
+
+			});
+		}
+
+		function sendDrinkTransaction(data, callback) {
+			var date = new Date(); 
+			var timeStamp = date.getDate() + "/"
+                + (date.getMonth()+1)  + "/" 
+                + date.getFullYear() + " @ "  
+                + date.getHours() + ":"  
+                + date.getMinutes() + ":" 
+                + date.getSeconds();
+			var message = {
+				"user" : userObject
+				"transaction" : {
+					"datetime" : timeStamp,
+					"data" : data
+				}
+			};
+			$.post(host + "/drink", message, callback);
+		}
+		
 	}
 }());
